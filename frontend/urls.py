d5d16b2039046d18bd191d6e7a28ec496e42889b
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-10-09 23:32:59 +0545

    dynamic links added and other pages made dynamic

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, about, contact, reservation
 
 app_name = 'frontend'
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('reservation/', reservation, name='reservation'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'frontend/index.html')
+
+def about(request):
+    return render(request, 'frontend/about.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
+
+def reservation(request):
+    return render(request, 'frontend/reservation.html')
